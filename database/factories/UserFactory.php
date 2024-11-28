d70e4531561dfe83293502bbe2d9b5ea223223bc
guitiorr <officialglude12345@gmail.com>
2024-11-28 21:42:39 +0700

    add user seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            // 'name' => fake()->name(),
+            'UserName' => fake()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,12 +2,17 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+
+        User::factory(20)->create();
+
         $this->call([
             CompanySeeder::class,
             JobSeeder::class,
